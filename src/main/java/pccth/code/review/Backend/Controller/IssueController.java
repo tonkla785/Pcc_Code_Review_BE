0a3c074341a6d37154ea2320ceb37d3962742090
Phattharaphon Puangngam <taowycsgo@gmail.com>
2026-01-05 14:17:45 +0700

    mock api Issue Management

--- src/main/java/pccth/code/review/Backend/Controller/IssueController.java
+++ src/main/java/pccth/code/review/Backend/Controller/IssueController.java
@@ -1,4 +1,39 @@
 package pccth.code.review.Backend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pccth.code.review.Backend.Service.IssueService;
+
+@RestController
+@RequestMapping("api")
 public class IssueController {
+    @Autowired
+    private IssueService issueService;
+
+
+    @GetMapping("/issues")
+    public String getListIssues(){
+        return issueService.listIssues();
+    }
+
+    @GetMapping("/issues/{id}")
+    public String getIssuesDetail(){
+        return issueService.getIssuesDetail();
+    }
+    @PutMapping("/issues/{id}/assign")
+    public String putAssignDeveloper(){
+        return issueService.assignDeveloper();
+    }
+    @PostMapping("/issues/{id}/comments")
+    public String postAddComment(){
+        return issueService.addComment();
+    }
+    @PutMapping("/issues/{id}/status")
+    public String putUpDateStatus(){
+        return issueService.upDateStatus();
+    }
+
+
+
+
 }
--- src/main/java/pccth/code/review/Backend/Service/IssueService.java
+++ src/main/java/pccth/code/review/Backend/Service/IssueService.java
@@ -2,6 +2,26 @@ package pccth.code.review.Backend.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class IssueService {
+
+
+    public String listIssues(){
+        return "List issues";
+    }
+    public String getIssuesDetail(){
+        return "Get issue details";
+    }
+    public String assignDeveloper(){
+        return "Assign developer";
+    }
+    public String addComment(){
+        return "Add comment";
+    }
+    public String upDateStatus(){
+        return "Update status";
+    }
+
 }
