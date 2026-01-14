f32a5153ef4945e42b8c856d402cf7555df44062
Bunlung777 <145154722+Bunlung777@users.noreply.github.com>
2026-01-14 09:49:01 +0700

    Update ProjectService.java

--- src/main/java/pccth/code/review/Backend/Service/ProjectService.java
+++ src/main/java/pccth/code/review/Backend/Service/ProjectService.java
@@ -1,5 +1,6 @@
 package pccth.code.review.Backend.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -7,6 +8,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import pccth.code.review.Backend.DTO.Request.RepositoryDTO;
+import pccth.code.review.Backend.DTO.Response.ProjectResponseDTO;
 import pccth.code.review.Backend.DTO.Response.RepositoryResponseDTO;
 import pccth.code.review.Backend.Entity.ProjectEntity;
 import pccth.code.review.Backend.EnumType.ProjectTypeEnum;
@@ -43,20 +45,43 @@ public class ProjectService {
     }
 
     // ดึงข้อมูล repository ทั้งหมด
-    public List<ProjectEntity> listProjects() {
-        return projectRepository.findAll();
+    public List<ProjectResponseDTO> listProjects() {
+        List<ProjectEntity> projects = projectRepository.findAll();
+        List<ProjectResponseDTO> dtoList = new ArrayList<>();
+
+        for (ProjectEntity p : projects) {
+            ProjectResponseDTO dto = new ProjectResponseDTO();
+            dto.setId(p.getId());
+            dto.setName(p.getName());
+            dto.setRepositoryUrl(p.getRepositoryUrl());
+            dto.setProjectType(p.getProjectType());
+            dto.setSonarProjectKey(p.getSonarProjectKey());
+            dto.setCreatedAt(p.getCreatedAt());
+            dto.setUpdatedAt(p.getUpdatedAt());
+            dtoList.add(dto);
+        }
+
+        return dtoList;
     }
 
     // ดึงข้อมูล repository ตาม id
-    public ProjectEntity searchRepository(UUID id) {
+    public ProjectResponseDTO searchRepository(UUID id) {
 
         ProjectEntity entity = projectRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Repository not found"));
 
-        return entity;
+        ProjectResponseDTO response = new ProjectResponseDTO();
+        response.setId(entity.getId());
+        response.setName(entity.getName());
+        response.setRepositoryUrl(entity.getRepositoryUrl());
+        response.setProjectType(entity.getProjectType());
+        response.setSonarProjectKey(entity.getSonarProjectKey());
+        response.setCreatedAt(entity.getCreatedAt());
+        response.setUpdatedAt(entity.getUpdatedAt());
+        return response;
     }
 
-    // แก้ไข repository
+    // แก้ไข repository จาก user
     public RepositoryResponseDTO updateRepository(UUID id, RepositoryDTO repository) {
         ProjectEntity entity = projectRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Repository not found")); // ตรวจสอบว่ามี id มั้ย
@@ -66,11 +91,28 @@ public class ProjectService {
             entity.setProjectType(ProjectTypeEnum.valueOf(repository.getType()));
             entity.setSonarProjectKey(repository.getName().trim().replaceAll("\\s+", "-"));
             entity.setUpdatedAt(new Date());
+            projectRepository.save(entity);
+
+            RepositoryResponseDTO response = new RepositoryResponseDTO();
+            response.setMessage("Repository updated successfully");
+            return response;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Server Error", e);
+        }
+    }
+
+    // แก้ไข repository จากการ scan
+    public RepositoryResponseDTO updateScanAt(UUID id) {
+        ProjectEntity entity = projectRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Repository not found")); // ตรวจสอบว่ามี id มั้ย
+        try {
+            entity.setUpdatedAt(new Date());
 
             projectRepository.save(entity);
 
             RepositoryResponseDTO response = new RepositoryResponseDTO();
-            response.setMessage("Repository added successfully");
+            response.setMessage("Repository updated successfully");
             return response;
 
         } catch (Exception e) {
