f36010dae10cf277c218954a0c6b86ab70221d93
Bunlung777 <145154722+Bunlung777@users.noreply.github.com>
2026-01-14 09:47:37 +0700

    Refactor WebhookController to use constructor injection

--- src/main/java/pccth/code/review/Backend/Controller/WebhookController.java
+++ src/main/java/pccth/code/review/Backend/Controller/WebhookController.java
@@ -1,34 +1,32 @@
 package pccth.code.review.Backend.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pccth.code.review.Backend.DTO.Response.N8NResponseDTO;
 import pccth.code.review.Backend.DTO.Response.ScanResponseDTO;
+import pccth.code.review.Backend.Service.ProjectService;
 import pccth.code.review.Backend.Service.ScanService;
 
 @RestController
 @RequestMapping("/webhooks")
+
 public class WebhookController {
-    @Autowired
-    private ScanService scanService;
+    private final ProjectService projectService;
+    private final ScanService scanService;
+
+    public WebhookController(ProjectService projectService, ScanService scanService) {
+        this.projectService = projectService;
+        this.scanService = scanService;
+    }
+
     @PostMapping("/scan/result")
     public ResponseEntity<N8NResponseDTO> receiveScanResult(
-            @RequestBody N8NResponseDTO result
-    ) {
+            @RequestBody N8NResponseDTO result) {
+
         System.out.println(result); // Result from sonarqube
-        ScanResponseDTO response = scanService.updateScan(result);
         // Add Logic Here
-        return ResponseEntity.ok(result);
-    }
-    @PostMapping("/scan/test")
-    public ResponseEntity<N8NResponseDTO> receiveScanResultTest(
-            @RequestBody N8NResponseDTO result
-    ) {
-        System.out.println(result); // Result from sonarqube
-        ScanResponseDTO response = scanService.updateScan(result);
-        // Add Logic Here
+        projectService.updateScanAt(result.getProjectId());
+        scanService.updateScan(result);
         return ResponseEntity.ok(result);
     }
 }
-
