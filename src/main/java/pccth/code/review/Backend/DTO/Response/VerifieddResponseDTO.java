1910dff122190355e343766a832a258bc3cd021c
Tanannat ( Austin ) <systemofgodcoednature@gmail.com>
2026-02-10 13:35:40 +0700

    Stogcn (#114)
    * ok
    
    * 001
    
    * add websocket to realtime verify user email

---
+++ src/main/java/pccth/code/review/Backend/DTO/Response/VerifieddResponseDTO.java
@@ -0,0 +1,62 @@
+package pccth.code.review.Backend.DTO.Response;
+
+public class VerifieddResponseDTO {
+    private String id;
+    private String username;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private String phone;
+    private String role;
+    private String status;
+
+
+
+}
