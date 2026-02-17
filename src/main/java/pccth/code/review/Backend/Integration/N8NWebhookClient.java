acf3cdcba0054856c023db2fb487d48b7a965f6f
tonkla785 <156068067+tonkla785@users.noreply.github.com>
2026-02-17 16:09:58 +0700

    Fix/backend sql (#122)
    * add analysis log finish
    
    * fix date at docker when scan update
    
    * edit security config
    
    * add status user
    
    * add email verification
    
    * add table notification setting sonarqube notification
    
    * add api for keep setting user / notification report history
    
    * edit create sonar setting
    
    * no message
    
    * no message
    
    * edit report history && clean all file
    
    * edit field data report
    
    * no message
    
    * no message
    
    * edit status user
    
    * no message
    
    * no message
    
    * no message
    
    * edit report generate at
    
    * no message
    
    * edit noti for comment noti
    
    * no message
    
    * add schema for report
    
    * add field report history
    
    * websocket realtime data v1
    
    * edit websocket
    
    * add field cost dev
    
    * add noti assign issue
    
    * add noti issue multi user
    
    * edit security
    
    * no message
    
    * edit issue upsert
    
    * edit sonar scanner
    
    * add generate ai recommend fix && websocket redis realtime update data to front end

--- src/main/java/pccth/code/review/Backend/Integration/N8NWebhookClient.java
+++ src/main/java/pccth/code/review/Backend/Integration/N8NWebhookClient.java
@@ -1,5 +1,6 @@
 package pccth.code.review.Backend.Integration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,9 @@ public class N8NWebhookClient {
 
     private final WebhookConfig webhookConfig;
     private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${n8n.recommend.ai}")
+    private String n8nUrlAi;
 
     public N8NWebhookClient(WebhookConfig webhookConfig) {
         this.webhookConfig = webhookConfig;
@@ -24,6 +28,19 @@ public class N8NWebhookClient {
 
         restTemplate.postForEntity(
                 webhookConfig.getWorkerUrl(),
+                entity,
+                Void.class
+        );
+    }
+
+    public <T> void postIssueToN8N(T payload){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("WEBHOOK-TOKEN", webhookConfig.getWebhookToken());
+
+        HttpEntity<T> entity = new HttpEntity<>(payload, headers);
+
+        restTemplate.postForEntity(
+                n8nUrlAi,
                 entity,
                 Void.class
         );
